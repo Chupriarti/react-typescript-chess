0b65774d2d896e92ea4672ac2baec246357af357
Chupriarti <wallx13@gmail.com>
2022-06-14 23:51:41 +0300

    add FigureNames

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,13 @@
+export enum FigureNames {
+  FIGURE = "Figure",
+  KING = "King",
+  KNIGHT = "Knight",
+  PAWN = "Pawn",
+  QUEEN = "Queen",
+  ROOK = "Rook",
+  BISHOP = "Bishop"
+}
+
 export class Figure {
 
 }
