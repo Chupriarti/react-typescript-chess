ca91360d7fc3285d27c6b45c4fd8da03e07bb1aa
Chupriarti <wallx13@gmail.com>
2022-06-18 22:27:04 +0300

    add current player logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
 import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Player';
 
 function App() {
   const [board, setBoard] = React.useState(new Board());
+  const [whitePlayer, setWhitePlayer] = React.useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = React.useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = React.useState<Player | null>(null);
 
   React.useEffect(() => {
     restart();
+    setCurrentPlayer(whitePlayer);
   }, [])
 
   function restart() {
@@ -17,11 +23,17 @@ function App() {
     setBoard(newBoard); 
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+  }
+
   return (
     <div className="App">
       <BoardComponent 
         board = {board}
         setBoard = {setBoard}
+        currentPlayer = {currentPlayer}
+        swapPlayer = {swapPlayer}
       />
     </div>
   );
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Board } from '../models/Board';
 import { Cell } from '../models/Cell';
+import { Player } from '../models/Player';
 import CellComponent from './CellComponent';
 
 interface BoardProps {
   board: Board;
   setBoard: (board: Board) => void;
+  currentPlayer: Player | null;
+  swapPlayer: () => void;
 }
 
-const BoardComponent: React.FC<BoardProps> = ({board, setBoard}) => {
+const BoardComponent: React.FC<BoardProps> = ({board, setBoard, currentPlayer, swapPlayer}) => {
   const [selectedCell, setSelectedCell] = React.useState<Cell | null>(null)
 
   function click(cell: Cell) {
     if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
       selectedCell.moveFigure(cell);
+      swapPlayer();
       setSelectedCell(null);
       updateBoard();
     } else {
-      setSelectedCell(cell);
+      if (cell.figure?.color === currentPlayer?.color){
+        setSelectedCell(cell);
+      }
     }
   }
 
